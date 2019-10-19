2336a6741ae7034f7855db796b35ef7634dcff19
mergitto <merugitosu@gmail.com>
2019-10-19 22:59:09 +0900

    特定のsocketのデータを抜き出す処理を追加

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ wss.on('connection', ws => {
         console.log('message:', message);
         connections.forEach((con, i) => {
             // con.send(i);
+            getLastLocation(100)
             con.send(message);
         });
     });
@@ -35,3 +36,26 @@ wss.on('connection', ws => {
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
+
+function getLastLocation(socket_id) {
+    const request = require('request');
+
+    const url = encodeURI(`https://packman.cybozu.com/k/v1/records.json?app=5&query=socket_id=${socket_id}`);
+    let params = {
+        url: url,
+        method: 'GET',
+        json: true,
+        // 本当はtokenを乗せてはいけない
+        headers: {
+            'X-Cybozu-API-Token': 'LPjpRT5Ekix06RdzJK78cwLpg0VlY2vrdYqA8zGi',
+        },
+    };
+
+    request(params, function (err, resp, body) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log(body);
+    });
+};
